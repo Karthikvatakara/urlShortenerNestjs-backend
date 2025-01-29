612922cc0a45b478305f70829a26441b8b4138ed
karthik <krishnankarthik07@gmail.com>
2025-01-29 11:39:49 +0530

    url changed

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: [process.env.URL],
     credentials: true,
     methods : ['GET','POST','PUT','DELETE'],
     allowedHeaders: ['Content-Type','Authorization']
